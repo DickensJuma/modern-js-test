909beba2cc9d5dd55ea52558578523bececea807
DickensJuma <48822462+DickensJuma@users.noreply.github.com>
2019-03-29 17:19:51 +0300

    file dublicated

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-function calculateDogAge(your_puppys_age){
-    var dogsAge = your_puppys_age * 7;
-    console.log(`Your doggie is ${dogsAge} years old in dog years!`);
-}
-calculateDogAge(12)
-calculateDogAge(1)
-calculateDogAge(2)
